27022d6827cf254ef9ef38b206e3f7df72caa438
coiz <coiz@192.168.178.39>
2019-08-23 14:19:47 +0200

    Modified Exercise 5-3 and 5-4

--- Learning Python/Chapter 05/Exercise 5-3.py
+++ Learning Python/Chapter 05/Exercise 5-3.py
@@ -1,11 +1,11 @@
 def check_fermat():
     prompt = "This program will check if Fermat's Last Theorem Holds True. Please provide an integer number!\n"
     a = int(input(prompt))
-    prompt = "Please provide an integer number!\n"
+    prompt = "Please provide another integer number!\n"
     b = int(input(prompt))
-    prompt = "Please provide an integer number!\n"
+    prompt = "Please provide another integer number!\n"
     c = int(input(prompt))
-    prompt = "Please provide an integer number!\n"
+    prompt = "Please provide an integer number greater than 2!\n"
     n = int(input(prompt))
     print('Now we will check if ' + str(a) + '^' + str(n) + ' + '+ str(b) + '^' + str(n) +' = '+ str(c) + '^' + str(n))
     if a**n+b**n == c**n:
