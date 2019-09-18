c849f5401398c74f6bb29c5d65a9b12c90cf5471
coiz <coiz@192.168.178.39>
2019-09-19 00:18:29 +0200

    Added ability to create 5 letter combination list

--- Learning Python/Chapter 09/Exercise93.py
+++ Learning Python/Chapter 09/Exercise93.py
@@ -1,3 +1,5 @@
+import itertools
+
 def avoid(word,string):
     for letter in string:
         #print(word, string, letter)
@@ -38,9 +40,32 @@ def check_list():
 
 #Out of 113809 words there are 37641 words without an e which is 33.07% - Verified
 
-check_list()
+#check_list()
 
+def combination_of_letters():
+    list = itertools.combinations('abcdefghijklmnopqrstuvwxyz', 5)
+    maximumHasNoLetterFromStringCounter = 0
+    for items in list:
+        string = items[0] + items[1] + items[2] + items[3] + items[4]
+        if check_list2(string) > maximumHasNoLetterFromStringCounter:
+            maximumHasNoLetterFromStringCounter = check_list2(string)
+            maximumString = string
+        print(maximumHasNoLetterFromStringCounter, maximumString, string)
+    
 '''
 To Do:
 Can you find a combination of 5 forbidden letters that excludes the smallest number of words?
 '''
+
+def check_list2(string):
+    totalCounter = 0
+    hasNoLetterFromStringCounter = 0
+    wordList = open('words.txt')
+    for line in wordList:
+        word = line.strip()
+        if avoid(word,string) == True:
+            hasNoLetterFromStringCounter = hasNoLetterFromStringCounter + 1
+        totalCounter = totalCounter + 1
+    return hasNoLetterFromStringCounter
+    
+combination_of_letters()
