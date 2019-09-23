bdf0414e673aec6295c7e9ca7538e61efbed2f30
coiz <coiz@192.168.178.39>
2019-09-23 21:12:20 +0200

    Added uses_all2 to Exercise95

--- Learning Python/Chapter 09/Exercise95.py
+++ Learning Python/Chapter 09/Exercise95.py
@@ -1,3 +1,9 @@
+def uses_only2(word, available):
+    for letter in word:
+        if letter not in available:
+            return False
+    return True
+
 def uses_all(word, string):
     word = word.lower()
     lengthString = len(string)
@@ -20,4 +26,5 @@ def traverse_list(string):
         if uses_all(word, string) == True:
             print(word)
             
-traverse_list('aeiou')
+def uses_all2(word, required):
+    return uses_only2(required, word)
