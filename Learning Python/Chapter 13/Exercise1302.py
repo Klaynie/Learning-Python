aa487815b4139176bdad72440892e6a8c4f31616
Christoph Lenz <coiz@hotmail.de>
2019-12-30 11:16:22 +0100

    Added Exercise1302

---
+++ Learning Python/Chapter 13/Exercise1302.py
@@ -0,0 +1,26 @@
+import string
+
+def generateWordDictionairy(fileName):
+    d = {}
+    wordCounter = 0
+    file = open(fileName)
+    for line in file:
+        line = line.lower()
+        line = line.translate({ord(i): ' ' for i in string.punctuation})
+        lineLength = len(line)
+        letterCounter = 0
+        word = ''
+        while letterCounter < lineLength:
+            if line[letterCounter] == ' ':
+                word = word.strip()
+                if len(word) != 0:
+                    d[word] = 1 + d.get(word, 0)
+                    wordCounter += 1
+                word = ''
+            if line[letterCounter].isalpha():
+                word += line[letterCounter]
+            letterCounter += 1
+    print(wordCounter)            
+    return d
+
+print(generateWordDictionairy('English.txt'))
