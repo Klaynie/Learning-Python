73522f8da021cfccd97962ecf736079bb8cc5d5a
Christoph Lenz <coiz@hotmail.de>
2020-02-24 20:01:25 +0100

    Changed Markov Analysis + Random Sentence Function

--- Learning Python/Chapter 13/Exercise1308.py
+++ Learning Python/Chapter 13/Exercise1308.py
@@ -4,7 +4,7 @@ wordCounterDict = 0
 
 def process_file_markov(filename, prefixLength):
     '''
-    This function will take a text file and markov analysis. It will take prefixLength words as the key for a dictionairy and will populate all unique words
+    This function will take a text file and perform a Markov analysis. It will take prefixLength words as the key for a dictionairy and will populate all unique words
     that follow after this key word tuple via a list
     
     filename: Name of the file
@@ -30,7 +30,7 @@ def process_file_markov(filename, prefixLength):
         else:
             if positionDictionariy.get(i+j) not in markovDictionariy[prefixTuple]:
                 markovDictionariy[prefixTuple].append(positionDictionariy.get(i+j))
-        i += j+1
+        i += 1
     return markovDictionariy
 
 def process_line(line, positionDictionariy):
@@ -45,14 +45,37 @@ def process_line(line, positionDictionariy):
 
 def createRandomSenctenceFromMarkovDictionariy(markovDictionariy, numberOfWords):
     sentence = ''
+    prefixList = []
+    prefixTuple = tuple()
+    # Seed the sentence with the a first randomly selected Markov Pair
     i = 0
+    randomKey = random.choice(list(markovDictionariy.keys()))
+    randomWord = random.choice(markovDictionariy[randomKey])
+    for word in randomKey:
+        sentence += word + ' '
+        '''
+        Taking all words after the first for the 2nd Key
+        '''
+        if i > 0:
+            prefixList.append(word)
+        i += 1
+    sentence += randomWord + ' '
+    prefixList.append(randomWord) # Randomly selected word from the list forms the last part of the 2nd Key 
+    prefixTuple = tuple(prefixList)
+    i += 1
+    '''
+    Create the rest of the sentence by using the a combination of the first Markov Pair with the randomly selected partner
+    '''
     while i < numberOfWords:
-        randomKey = random.choice(list(markovDictionariy.keys()))
-        randomWord = random.choice(markovDictionariy[randomKey])
-        for word in randomKey:
-            sentence += word + ' '
-            i += 1
+        randomWord = random.choice(markovDictionariy[prefixTuple])
         sentence += randomWord + ' '
+        '''
+        Prepare the next key
+        '''
+        del prefixList[0]
+        prefixTuple = tuple()
+        prefixList.append(randomWord)
+        prefixTuple = tuple(prefixList)
         i += 1
     print(sentence)
 
