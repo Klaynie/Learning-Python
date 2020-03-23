baaafab183713b94e83409a13a240ffb3571fa46
Christoph Lenz <coiz@hotmail.de>
2020-03-23 16:03:07 +0100

    Added Exercise1601

---
+++ Learning Python/Chapter 16/Exercise1601.py
@@ -0,0 +1,13 @@
+class Time(object):
+    """Represents the time of day.
+    attributes: hour, minute, second
+    """
+def print_time(time):
+    print('%.2d:%.2d:%.2d' % (time.hour, time.minute, time.second))
+
+time = Time()
+time.hour = 11
+time.minute = 59
+time.second = 30
+
+print_time(time)
