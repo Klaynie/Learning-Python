3f19a1bf08e505223ce1dd6cc689bfcaea46070a
Christoph Lenz <coiz@hotmail.de>
2020-03-23 13:13:33 +0100

    Did Step 3 of Exercise 1504

--- Learning Python/Swampy/python3/Exercise1504.py
+++ Learning Python/Swampy/python3/Exercise1504.py
@@ -11,7 +11,14 @@ except ImportError:
     from World import *
 
 class Point(object):
-    """Represents a point in 2-D space."""
+    """Represents a point in 2-D space.
+    attributes: x, y, color.
+    """
+
+class Circle(object):
+    """"Represents a circle.
+    attributes: center, radius, color.
+    """
 
 class Rectangle(object):
     """Represents a rectangle.
@@ -19,8 +26,11 @@ class Rectangle(object):
     """
 
 def draw_rectangle(canvas, rect):
-    bbox = [[-rect.width/2,-rect.height/2], [rect.width/2, rect.height/2]]
-    canvas.rectangle(bbox, outline='black', width=2, fill= box.color)
+    bbox = [[rect.corner.x,rect.corner.y], [rect.width + rect.corner.x, rect.height + rect.corner.y]]
+    canvas.rectangle(bbox, outline='black', width=2, fill= rect.color)
+
+def draw_point(canvas, point):
+    canvas.circle([point.x,point.y], 1, outline='black', fill= point.color)
 
 world = World()
 canvas = world.ca(width=500, height=500, background='white')
@@ -33,5 +43,33 @@ box.corner.x = 0
 box.corner.y = 0
 box.color = 'blue1'
 
+box2 = Rectangle()
+box2.corner = Point()
+box2.width = 100.0
+box2.height = 200.0
+box2.corner.x = -100
+box2.corner.y = -200
+box2.color = 'green1'
+
+point = Point()
+point.x = 50
+point.y = 50
+point.color = 'red'
+
+point2 = Point()
+point2.x = -50
+point2.y = -50
+point2.color = 'yellow'
+
+circle = Circle()
+circle.center = Point()
+circle.radius = 100
+circle.center.x = 100
+circle.center.y = 100
+circle.color = 'snow'
+
 draw_rectangle(canvas, box)
+draw_rectangle(canvas, box2)
+draw_point(canvas, point)
+draw_point(canvas, point2)
 world.mainloop()
