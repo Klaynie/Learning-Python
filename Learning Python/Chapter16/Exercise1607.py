ec4cd682cc5f30acce4793c52f67854538097faa
Christoph Lenz <coiz@hotmail.de>
2020-03-24 11:35:51 +0100

    Refactored Exercise1607

--- Learning Python/Chapter16/Exercise1607.py
+++ Learning Python/Chapter16/Exercise1607.py
@@ -2,57 +2,51 @@ import datetime
 import math
 import copy
 
+def get_timestamp():
+    return datetime.datetime.utcnow()
+
 def weekday_of_today():
-    timestamp = datetime.datetime.utcnow()
+    timestamp = get_timestamp()
     return timestamp.weekday()
 
-def age_from_birthday(birthday):
-    delta = datetime.datetime.utcnow() - birthday
-    ageYears = int(math.floor(delta.days / 365.2425))
-    return ageYears
+def calculate_age_between_two_dates(date1, date2):
+    return date2.year - date1.year - ((date2.month, date2.day) < (date1.month, date1.day))
+
+def current_age_from_birthday(birthday):
+    return calculate_age_between_two_dates(birthday, get_timestamp())
 
 def age_until_date(birthday, date):
-    delta = date - birthday + datetime.timedelta(days = 1)
-    ageYears = int(math.floor(delta.days / 365.2525))
-    return ageYears
+    return calculate_age_between_two_dates(birthday, date)
 
-def time_until_date(date):
-    time = date - datetime.datetime.utcnow()
-    return time
+def time_until_date(date, timestamp):
+    return date - timestamp
 
-def get_time_to_next_birthday(currentTimestamp, birthday):
+def get_time_to_next_birthday(birthday, currentTimestamp):
     if birthday.month <= currentTimestamp.month and (birthday.month <= currentTimestamp.month and birthday.day <= currentTimestamp.day):
-        return time_until_date(datetime.datetime(currentTimestamp.year+1, birthday.month, birthday.day))
+        return time_until_date(datetime.datetime(currentTimestamp.year+1, birthday.month, birthday.day), currentTimestamp)
     else:
-        return time_until_date(datetime.datetime(currentTimestamp.year, birthday.month, birthday.day))
+        return time_until_date(datetime.datetime(currentTimestamp.year, birthday.month, birthday.day), currentTimestamp)
 
 def print_age_and_time_to_next_birthday(birthday):
-    currentTimestamp = datetime.datetime.utcnow()
-    print('You are %g years old. Time to next birthday: %s' % (age_from_birthday(birthday), get_time_to_next_birthday(currentTimestamp, birthday)))
+    print('You are %g years old. Time to next birthday: %s' % (current_age_from_birthday(birthday), get_time_to_next_birthday(birthday, get_timestamp())))
+
+def find_older_date(date1, date2):
+    if date2 > date1:
+        return date1, date2
+    else:
+        return date2, date1
 
 def find_double_date(birthday1, birthday2, n):
-    if birthday2 > birthday1:
-        i = 0
-        newDate = copy.deepcopy(birthday2)
-        nextDay = newDate + datetime.timedelta(days = i)
-        while age_until_date(birthday1, nextDay) != age_until_date(birthday2, nextDay)*n:
-            nextDay = newDate + datetime.timedelta(days = i)
-            i += 1
-        print(nextDay, age_until_date(birthday1, nextDay),age_until_date(birthday2, nextDay))
-    else:
-        i = 0
-        newDate = copy.deepcopy(birthday1)
-        nextDay = newDate + datetime.timedelta(days = i)
-        while age_until_date(birthday2, nextDay) != age_until_date(birthday1, nextDay)*n:
-            nextDay = newDate + datetime.timedelta(days = i)
-            i += 1
-        print(nextDay, age_until_date(birthday1, nextDay),age_until_date(birthday2, nextDay))
-
-#birthday = datetime.datetime(1986,11,8)
-
-#print_age_and_time_to_next_birthday(birthday)
+    olderBirthday, youngerBirthday = find_older_date(birthday1, birthday2)
+    nextDayCounter = 0
+    nextDay = copy.deepcopy(youngerBirthday)
+    while age_until_date(olderBirthday, nextDay) != age_until_date(youngerBirthday, nextDay)*n:
+        nextDay += datetime.timedelta(days = 1)
+        nextDayCounter += 1
+    print(nextDay, age_until_date(olderBirthday, nextDay), age_until_date(youngerBirthday, nextDay))
 
 birthday1 = datetime.datetime(2000,11,15)
-birthday2 = datetime.datetime(1986,7,5)
+birthday2 = datetime.datetime(1977,2,2)
 
-find_double_date(birthday1, birthday2, 4)
+print_age_and_time_to_next_birthday(birthday1)
+find_double_date(birthday1, birthday2, 3)
