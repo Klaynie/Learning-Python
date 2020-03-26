3c9ba4db4a8f8dc249de2a94d310fcbf7c0cf36f
Christoph Lenz <coiz@hotmail.de>
2020-03-26 13:54:58 +0100

    Added Exercise1802

---
+++ Learning Python/Chapter18/Exercise1802.py
@@ -0,0 +1,64 @@
+"""
+
+Code example from Think Python, by Allen B. Downey.
+Available from http://thinkpython.com
+
+Copyright 2012 Allen B. Downey.
+Distributed under the GNU General Public License at gnu.org/licenses/gpl.html.
+
+"""
+import random
+
+class Card(object):
+    """Represents a standard playing card."""
+
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.rank = rank
+    
+    suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+    
+    def __str__(self):
+        return '%s of %s' % (Card.rank_names[self.rank], Card.suit_names[self.suit])
+
+    def __lt__(self, other):
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return t1 < t2
+
+class Deck(object):
+
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+    def pop_card(self):
+        return self.cards.pop()
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def sort(self):
+        return self.cards.sort()
+
+deck = Deck()
+print(deck)
+print('End of Initial')
+deck.shuffle()
+print(deck)
+print('End of Shuffel')
+deck.sort()
+print(deck)
