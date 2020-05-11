259b18fc699a7eacf7a83286881c0fcc69d28364
Christoph Lenz <coiz@hotmail.de>
2020-05-11 18:19:44 +0200

    First Version of 6521 Particles

---
+++ Learning Python/Hyperskill/6521-Particles.py
@@ -0,0 +1,15 @@
+input_1 = input()
+input_2 = input()
+
+if input_1 == "1/2" and input_2 == "-1/3":
+    print("Strange Quark")
+elif input_1 == "1/2" and input_2 == "2/3":
+    print("Charm Quark")
+elif input_1 == "1/2" and input_2 == "-1":
+    print("Electron Lepton")
+elif input_1 == "1/2" and input_2 == "0":
+    print("Muon Lepton")
+elif input_1 == "1" and input_2 == "0":
+    print("Photon Boson")
+elif input_1 == "0" and input_2 == "0":
+    print("Higgs boson Boson")
