f2d580ac26793a3863a414a71c3bd734ce809298
Christoph Lenz <coiz@hotmail.de>
2020-05-11 18:22:13 +0200

    Refactoring of 6521

--- Learning Python/Hyperskill/6521-Particles.py
+++ Learning Python/Hyperskill/6521-Particles.py
@@ -1,15 +1,15 @@
-input_1 = input()
-input_2 = input()
+spin = input()
+charge = input()
 
-if input_1 == "1/2" and input_2 == "-1/3":
+if spin == "1/2" and charge == "-1/3":
     print("Strange Quark")
-elif input_1 == "1/2" and input_2 == "2/3":
+elif spin == "1/2" and charge == "2/3":
     print("Charm Quark")
-elif input_1 == "1/2" and input_2 == "-1":
+elif spin == "1/2" and charge == "-1":
     print("Electron Lepton")
-elif input_1 == "1/2" and input_2 == "0":
+elif spin == "1/2" and charge == "0":
     print("Muon Lepton")
-elif input_1 == "1" and input_2 == "0":
+elif spin == "1" and charge == "0":
     print("Photon Boson")
-elif input_1 == "0" and input_2 == "0":
+elif spin == "0" and charge == "0":
     print("Higgs boson Boson")
