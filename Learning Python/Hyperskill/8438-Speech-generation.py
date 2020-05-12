9e201073b74742352910f5d63a9c2d3a2b501f6b
Christoph Lenz <coiz@hotmail.de>
2020-05-12 20:00:55 +0200

    Second Version of Speech Generation

--- Learning Python/Hyperskill/8438-Speech-generation.py
+++ Learning Python/Hyperskill/8438-Speech-generation.py
@@ -3,5 +3,4 @@ phone_number = input()
 number_translation = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 for number in phone_number:
-    number = int(number)
-    print(number_translation[number])
+    print(number_translation[int(number)])
