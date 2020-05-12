023787d5a180f15e41e560fb4db844d7645ac541
Christoph Lenz <coiz@hotmail.de>
2020-05-12 20:53:13 +0200

    Removed list in 6549

--- Learning Python/Hyperskill/6549-Lucky-7.py
+++ Learning Python/Hyperskill/6549-Lucky-7.py
@@ -1,8 +1,6 @@
 number_of_lines = int(input())
-user_input = []
 
 for line in range(0, number_of_lines):
-    user_input.append(int(input()))
-for number in user_input:
+    number = int(input())
     if number % 7 == 0:
         print(number**2)
