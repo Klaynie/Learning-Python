f80a0e3abee81c88bc49e35917ccccab06115f1c
Christoph Lenz <coiz@hotmail.de>
2020-05-12 20:55:16 +0200

    Changed for variable

--- Learning Python/Hyperskill/6549-Lucky-7.py
+++ Learning Python/Hyperskill/6549-Lucky-7.py
@@ -1,6 +1,6 @@
 number_of_lines = int(input())
 
-for line in range(0, number_of_lines):
+for number in range(0, number_of_lines):
     number = int(input())
     if number % 7 == 0:
         print(number**2)
