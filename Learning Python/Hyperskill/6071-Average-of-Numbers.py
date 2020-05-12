114663d8e7f0a35ce0b827a21a398a0c25a1cf7b
Christoph Lenz <coiz@hotmail.de>
2020-05-12 20:19:11 +0200

    Typo

--- Learning Python/Hyperskill/6071-Average-of-Numbers.py
+++ Learning Python/Hyperskill/6071-Average-of-Numbers.py
@@ -1,11 +1,11 @@
 interval_start = int(input())
 interval_end = int(input())
 
-divisiable_by_three_aggregator = 0
-divisiable_by_three_counter = 0
+divisible_by_three_aggregator = 0
+divisible_by_three_counter = 0
 
 for number in range(interval_start, interval_end + 1):
     if number % 3 == 0:
-        divisiable_by_three_aggregator += number
-        divisiable_by_three_counter += 1
-print(divisiable_by_three_aggregator / divisiable_by_three_counter)
+        divisible_by_three_aggregator += number
+        divisible_by_three_counter += 1
+print(divisible_by_three_aggregator / divisible_by_three_counter)
