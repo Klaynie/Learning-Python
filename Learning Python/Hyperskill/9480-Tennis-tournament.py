5aaf20514478fc0c5305d9080bcb795047e05388
Christoph Lenz <coiz@hotmail.de>
2020-05-13 12:13:34 +0200

    Refactor of 9480

--- Learning Python/Hyperskill/9480-Tennis-tournament.py
+++ Learning Python/Hyperskill/9480-Tennis-tournament.py
@@ -1,11 +1,10 @@
 number_of_inputs = int(input())
 output_list = []
 
-for combined_user_input in range(number_of_inputs):
-    combined_user_input = input()
-    input_list = combined_user_input.split()
-    if input_list[1] == "win":
-        output_list.append(input_list[0])
+for _ in range(number_of_inputs):
+    name, outcome = input().split()
+    if outcome == "win":
+        output_list.append(name)
 
 print(output_list)
 print(len(output_list))
