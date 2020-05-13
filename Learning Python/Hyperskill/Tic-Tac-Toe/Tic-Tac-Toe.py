448cecde92cc440a8bf6d78e67aec3078eacd5f4
Christoph Lenz <coiz@hotmail.de>
2020-05-13 15:59:42 +0200

    Tic-Tac-Toe Generate Field

---
+++ Learning Python/Hyperskill/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -0,0 +1,6 @@
+cells = input("Enter cells:")
+print("---------")
+print("| " + cells[0] + " " + cells[1] + " " + cells[2] + " |")
+print("| " + cells[3] + " " + cells[4] + " " + cells[5] + " |")
+print("| " + cells[6] + " " + cells[7] + " " + cells[8] + " |")
+print("---------")
