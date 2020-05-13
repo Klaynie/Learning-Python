4e7484489b7052bb8dd8acda3f673105eef4c3c0
Christoph Lenz <coiz@hotmail.de>
2020-05-13 18:20:11 +0200

    Second Version of Win Conditions

--- Learning Python/Hyperskill/Tic-Tac-Toe/Tic-Tac-Toe.py
+++ Learning Python/Hyperskill/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -9,47 +9,56 @@ row_1 = cells[0]+cells[1]+cells[2]
 row_2 = cells[3]+cells[4]+cells[5]
 row_3 = cells[6]+cells[7]+cells[8]
 
+rows = [row_1, row_2, row_3]
+
 column_1 = cells[0]+cells[3]+cells[6]
 column_2 = cells[1]+cells[4]+cells[7]
 column_3 = cells[2]+cells[5]+cells[8]
 
+columns = [column_1, column_2, column_3]
+
 diagonal_1 = cells[0]+cells[4]+cells[8]
 diagonal_2 = cells[6]+cells[4]+cells[2]
+
+diagonals = [diagonal_1, diagonal_2]
+
+field_states = [rows, columns, diagonals]
 
 win_condition_x = 'XXX'
 win_condition_o = 'OOO'
 
-if cells == '_________':
+count_x = 0
+count_o = 0
+count_win_conditions = 0
+count_x_wins = 0
+count_o_wins = 0
+
+for symbol in cells:
+    if symbol == 'X':
+        count_x += 1
+    if symbol == 'O':
+        count_o += 1
+
+for lists in field_states:
+    for items in lists:
+        if items == win_condition_x:
+            count_x_wins += 1
+            count_win_conditions += 1
+        elif items == win_condition_o:
+            count_o_wins += 1
+            count_win_conditions += 1
+
+if count_win_conditions > 1:
+    print("Impossible")
+elif count_o > count_x + 1 or count_x > count_o + 1:
+    print("Impossible")
+elif cells == '_________':
     print("New Game")
-elif row_1 == win_condition_x:
+elif count_x_wins == 1:
     print("X wins")
-elif row_2 == win_condition_x:
-    print("X wins")
-elif row_3 == win_condition_x:
-    print("X wins")
-elif column_1 == win_condition_x:
-    print("X wins")
-elif column_2 == win_condition_x:
-    print("X wins")
-elif column_3 == win_condition_x:
-    print("X wins")
-elif diagonal_1 == win_condition_x:
-    print("X wins")
-elif diagonal_2 == win_condition_x:
-    print("X wins")
-elif row_1 == win_condition_o:
+elif count_o_wins == 1:
     print("O wins")
-elif row_2 == win_condition_o:
-    print("O wins")
-elif row_3 == win_condition_o:
-    print("O wins")
-elif column_1 == win_condition_o:
-    print("O wins")
-elif column_2 == win_condition_o:
-    print("O wins")
-elif column_3 == win_condition_o:
-    print("O wins")
-elif diagonal_1 == win_condition_o:
-    print("O wins")
-elif diagonal_2 == win_condition_o:
-    print("O wins")
+elif '_' in cells:
+    print('Game not finished')
+else:
+    print("Draw")
