fff4f56bc37fd7c42a24ee5c21149853d656c9a7
Christoph Lenz <coiz@hotmail.de>
2020-05-13 17:45:21 +0200

    First Draft of Win Conditions

--- Learning Python/Hyperskill/Tic-Tac-Toe/Tic-Tac-Toe.py
+++ Learning Python/Hyperskill/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -4,3 +4,52 @@ print("| " + cells[0] + " " + cells[1] + " " + cells[2] + " |")
 print("| " + cells[3] + " " + cells[4] + " " + cells[5] + " |")
 print("| " + cells[6] + " " + cells[7] + " " + cells[8] + " |")
 print("---------")
+
+row_1 = cells[0]+cells[1]+cells[2]
+row_2 = cells[3]+cells[4]+cells[5]
+row_3 = cells[6]+cells[7]+cells[8]
+
+column_1 = cells[0]+cells[3]+cells[6]
+column_2 = cells[1]+cells[4]+cells[7]
+column_3 = cells[2]+cells[5]+cells[8]
+
+diagonal_1 = cells[0]+cells[4]+cells[8]
+diagonal_2 = cells[6]+cells[4]+cells[2]
+
+win_condition_x = 'XXX'
+win_condition_o = 'OOO'
+
+if cells == '_________':
+    print("New Game")
+elif row_1 == win_condition_x:
+    print("X wins")
+elif row_2 == win_condition_x:
+    print("X wins")
+elif row_3 == win_condition_x:
+    print("X wins")
+elif column_1 == win_condition_x:
+    print("X wins")
+elif column_2 == win_condition_x:
+    print("X wins")
+elif column_3 == win_condition_x:
+    print("X wins")
+elif diagonal_1 == win_condition_x:
+    print("X wins")
+elif diagonal_2 == win_condition_x:
+    print("X wins")
+elif row_1 == win_condition_o:
+    print("O wins")
+elif row_2 == win_condition_o:
+    print("O wins")
+elif row_3 == win_condition_o:
+    print("O wins")
+elif column_1 == win_condition_o:
+    print("O wins")
+elif column_2 == win_condition_o:
+    print("O wins")
+elif column_3 == win_condition_o:
+    print("O wins")
+elif diagonal_1 == win_condition_o:
+    print("O wins")
+elif diagonal_2 == win_condition_o:
+    print("O wins")
