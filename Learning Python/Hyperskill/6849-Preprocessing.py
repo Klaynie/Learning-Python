9bbf0ee0594d07155fce8a0d9213976909fda140
Christoph Lenz <coiz@hotmail.de>
2020-05-14 15:08:22 +0200

    Second Version of 6849

--- Learning Python/Hyperskill/6849-Preprocessing.py
+++ Learning Python/Hyperskill/6849-Preprocessing.py
@@ -8,5 +8,11 @@ Then print your text.
 
 Punctuation marks appear not only at the end of the input string, so you have to figure out how to get rid of all of them.
 """
+string = input()
 
-print(input().strip(',.!?').lower())
+punctionation_marks = ',.!?'
+
+for item in punctionation_marks:
+    string = string.replace(item, '')
+
+print(string.lower())
