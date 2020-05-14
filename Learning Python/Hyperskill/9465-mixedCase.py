29c073c99cbce8c9db9b145f7fa59ba8fa8d5f2e
Christoph Lenz <coiz@hotmail.de>
2020-05-14 16:21:41 +0200

    Refactor of 9465

--- Learning Python/Hyperskill/9465-mixedCase.py
+++ Learning Python/Hyperskill/9465-mixedCase.py
@@ -1,7 +1,7 @@
 word_list = input().split()
 
 new_word_list = [word_list[0].lower()]
-for number in range(1,len(word_list)):
+for number in range(1, len(word_list)):
     new_word_list.append(word_list[number].capitalize())
 
 print("".join(new_word_list))
