9d3819016e76239a099f3c3cc027ec5337cd7462
Christoph Lenz <coiz@hotmail.de>
2020-05-20 10:23:48 +0200

    First Version of Project Stage 2

--- Learning Python/Hyperskill/Coffee-Machine/Coffee-Machine.py
+++ Learning Python/Hyperskill/Coffee-Machine/Coffee-Machine.py
@@ -1,11 +1,22 @@
 # Write your code here
-output_messages = ["Starting to make a coffee",
-                   "Grinding coffee beans",
-                   "Boiling water",
-                   "Mixing boiled water with crushed coffee beans",
-                   "Pouring coffee into the cup",
-                   "Pouring some milk into the cup",
-                   "Coffee is ready!"]
-                   
-for message in output_messages:
-    print(message)
+user_prompt = ["Write how many cups of coffee you will need:"]
+output_messages = [ "Starting to make a coffee",
+                    "Grinding coffee beans",
+                    "Boiling water",
+                    "Mixing boiled water with crushed coffee beans",
+                    "Pouring coffee into the cup",
+                    "Pouring some milk into the cup",
+                    "Coffee is ready!"]
+                    
+def print_ingredient_messages(cups_of_coffee, amount_water, amount_milk, amount_coffee_beans):
+    print(f"For {cups_of_coffee} cups of coffee you will need:")
+    print(f"{amount_water} ml of water")
+    print(f"{amount_milk} ml of milk")
+    print(f"{amount_coffee_beans} g of coffee beans")
+
+def calculate_ingredient_amount(cups_of_coffee):
+    return cups_of_coffee * 200, cups_of_coffee * 50, cups_of_coffee * 15
+
+cups_of_coffee = int(input(user_prompt[0]))
+amount_water, amount_milk, amount_coffee_beans = calculate_ingredient_amount(cups_of_coffee)
+print_ingredient_messages(cups_of_coffee, amount_water, amount_milk, amount_coffee_beans)
