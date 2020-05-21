26841edca2bd88eea0b1783aec05159e15673d86
Christoph Lenz <coiz@hotmail.de>
2020-05-21 16:26:21 +0200

    6683

--- Learning Python/Hyperskill/6683-Sphere.py
+++ Learning Python/Hyperskill/6683-Sphere.py
@@ -3,7 +3,7 @@ class Sphere:
 
     def __init__(self, rad_):
         self.rad_ = rad_
-        self.volume = 4 / 3 * Sphere.pi_ * rad_ ** 3
+        self.volume = 4 / 3 * pi_ * rad_ ** 3
 
 sphere = Sphere(3)
 
