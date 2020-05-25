01e22681aee01d37499bfb78c695dea17d7d629d
Christoph Lenz <coiz@hotmail.de>
2020-05-25 18:07:53 +0200

    First Version of 2nd Project Stage

--- Learning Python/Hyperskill/Hangman/Hangman.py
+++ Learning Python/Hyperskill/Hangman/Hangman.py
@@ -1,5 +1,11 @@
-word = 'H A N G M A N'
-message = 'The game will be available soon.'
+title = 'H A N G M A N'
+user_message = 'Guess the word:'
+outcome_messages = ['You survived!', 'You are hanged!']
+word_to_guess = 'python'
+print(title)
 
-print(word)
-print(message)
+input_ = input(user_message)
+if input_ == word_to_guess:
+    print(outcome_messages[0])
+elif input_ != word_to_guess:
+    print(outcome_messages[1])
