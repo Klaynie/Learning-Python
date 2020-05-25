f7fe1383ffcaa33cfd8429e130286b32d1f45a8f
Christoph Lenz <coiz@hotmail.de>
2020-05-25 23:19:41 +0200

    First Version of 9500

---
+++ Learning Python/Hyperskill/9500-Markdown-heading.py
@@ -0,0 +1,13 @@
+def heading(header, level=1):
+    if level < 1:
+        return heading(header, level=1)
+    elif level > 6:
+        return heading(header, level=6)
+    else:
+        return f'{"#" * level} {header}'
+
+print(heading("A"))      # Returns "# A"
+print(heading("A", 3))   # Returns "### A"
+print(heading("A", 1))   # Returns "# A"
+print(heading("A", 0))   # Returns "# A"
+print(heading("A", 10))  # Returns "###### A"
