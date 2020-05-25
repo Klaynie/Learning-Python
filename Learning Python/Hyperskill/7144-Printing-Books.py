400c2ffca33a430b7f845e60f122fdfd5cbcf81f
Christoph Lenz <coiz@hotmail.de>
2020-05-25 08:53:07 +0200

    First Version of 7144

---
+++ Learning Python/Hyperskill/7144-Printing-Books.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, author, title, price, book_id):
+        self.author = author
+        self.title = title
+        self.price = price
+        self.book_id = book_id
+
+    def __str__(self):
+        return f"{self.title} by {self.author}. ${self.price}. [{self.book_id}]"
+
+new_book = Book('A', 'B', '3.50', '1')
+print(new_book)
