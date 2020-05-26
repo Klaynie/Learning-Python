5edcde17ace19339e066e422813f21797f9a2eab
Christoph Lenz <coiz@hotmail.de>
2020-05-26 17:40:30 +0200

    Correct Version of 9560

--- Learning Python/Hyperskill/9560-Simon-says.py
+++ Learning Python/Hyperskill/9560-Simon-says.py
@@ -1,8 +1,12 @@
 def what_to_do(instructions):
     string_to_check = "Simon says"
-    if string_to_check not in instructions:
-        print("I won't do it!")
-    else:
-        print("I", instructions[:instructions.index(string_to_check)])
+    if string_to_check not in instructions or not (instructions.startswith(string_to_check) or instructions.endswith(string_to_check)):
+        return "I won't do it!"
+    return_string = "I "
+    if instructions.startswith(string_to_check):
+        return_string += instructions[len(string_to_check):].strip()
+    elif instructions.endswith(string_to_check):
+        return_string += instructions[:instructions.index(string_to_check)].strip()
+    return return_string
 
-what_to_do(input())
+print(what_to_do("make Simon says a wish"))
