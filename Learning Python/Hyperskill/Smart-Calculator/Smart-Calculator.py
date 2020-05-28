a7ada5f74531989e22d2a90383d568502d27d35e
Christoph Lenz <coiz@hotmail.de>
2020-05-28 14:07:39 +0200

    First Version of Project Stage 2

--- Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
+++ Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
@@ -1,3 +1,13 @@
-input_list = input().split()
-number_list = [int(number) for number in input_list]
-print(number_list[0] + number_list[1])
+keep_going = True
+while keep_going:
+    input_ = input()
+    if input_ == '/exit':
+        keep_going = False
+    elif input_ == '':
+        pass
+    elif ' ' not in input_:
+        print(input_)
+    else:
+        input_list = input_.split()
+        number_list = [int(number) for number in input_list]
+        print(number_list[0] + number_list[1])
