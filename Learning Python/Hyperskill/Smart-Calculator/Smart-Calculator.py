9caf3d0edfe85dda8954939622879ff4122acdf5
Christoph Lenz <coiz@hotmail.de>
2020-05-28 20:13:01 +0200

    First Version of Project Stage 3

--- Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
+++ Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
@@ -1,14 +1,20 @@
+commands = ['/exit','/help']
+user_outputs = ['Bye!','The program calculates the sum of numbers']
+
+def input_handler(input_):
+    return sum([int(number) for number in input_.split()])
+
 keep_going = True
 while keep_going:
     input_ = input()
-    if input_ == '/exit':
-        print('Bye!')
+    if input_ == commands[0]:
+        print(user_outputs[0])
         keep_going = False
+    elif input_ == commands[1]:
+        print(user_outputs[1])
     elif input_ == '':
         pass
     elif ' ' not in input_:
         print(input_)
     else:
-        input_list = input_.split()
-        number_list = [int(number) for number in input_list]
-        print(number_list[0] + number_list[1])
+        print(input_handler(input_))        
