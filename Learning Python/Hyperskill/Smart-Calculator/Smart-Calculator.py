db36240d1ccb7bbc178a6047820ac73b9b9cc2e0
Christoph Lenz <coiz@hotmail.de>
2020-05-29 13:20:14 +0200

    First Version of Project Stage 4

--- Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
+++ Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
@@ -1,5 +1,5 @@
 commands = ['/exit','/help']
-user_outputs = ['Bye!','The program calculates the sum of numbers']
+user_outputs = ['Bye!','The program calculates the sum and difference of numbers']
 keep_going = True
 
 def is_empty_line(input_):
@@ -8,8 +8,40 @@ def is_empty_line(input_):
 def is_single_number(input_):
     return ' ' not in input_
 
-def sum_(input_):
-    return sum([int(number) for number in input_.split()])
+def convert_numbers_and_operators(start, end, numbers, operators, output_list):
+    for list_iterator in range(start, end):
+        output_list.append(numbers[list_iterator] * int(operators[list_iterator - start] + '1'))
+    return output_list
+
+def generate_output_list(numbers, operators):
+    output_list = []
+    if len(numbers) == len(operators):
+        convert_numbers_and_operators(start=0, end=len(numbers), numbers=numbers, operators=operators, output_list=output_list)
+    elif len(numbers) != len(operators):
+        output_list.append(numbers[0])
+        convert_numbers_and_operators(start=1, end=len(numbers), numbers=numbers, operators=operators, output_list=output_list)
+    return output_list
+
+def convert_input(input_):
+    if input_.startswith('-'):
+        input_ = input_.replace("-", "- ", 1)
+    items = [item for item in input_.split()]
+    numbers = [int(item) for item in items if item.isdigit()]
+    operators = [convert_operator_string(item) for item in items if not item.isdigit()]
+    return generate_output_list(numbers, operators)
+
+def convert_operator_string(operator_string):
+    if '-' not in operator_string:
+        return '+'
+    else:
+        count_minus_sign = 0
+        for sign in operator_string:
+            if sign == '-':
+                count_minus_sign += 1
+        if count_minus_sign % 2 == 0:
+            return '+'
+        else:
+            return '-'
 
 def input_handler(input_):
     global keep_going
@@ -22,8 +54,8 @@ def input_handler(input_):
         pass
     elif is_single_number(input_):
         print(input_)
-    else:
-        print(sum_(input_))
+    else:        
+        print(sum(convert_input(input_)))
 
 def calculator_loop():
     global keep_going
