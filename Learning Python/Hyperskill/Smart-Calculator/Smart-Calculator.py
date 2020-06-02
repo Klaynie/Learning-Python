440ed89db86a7897593e6575fc7718de6d2d060a
Christoph Lenz <coiz@hotmail.de>
2020-06-02 16:34:01 +0200

    First Version of Project Stage 5

--- Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
+++ Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
@@ -1,5 +1,7 @@
 commands = ['/exit','/help']
-user_outputs = ['Bye!','The program calculates the sum and difference of numbers']
+user_outputs = ['Bye!','The program calculates the sum and difference of numbers', 'Unknown command', 'Invalid expression']
+command_start_symbol = '/'
+operator_symbols = ['+', '-']
 keep_going = True
 
 def is_empty_line(input_):
@@ -64,19 +66,59 @@ def convert_operator_string(operator_string):
         else:
             return '-'
 
-def input_handler(input_):
-    global keep_going
+def command_handler(input_):
+    """ Checks if the command is in the list of commands
+
+    Returns the command description if available, otherwise will let the user know that know that command is unkown
+    """
+    global keep_going, commands
+    if input_ not in commands:
+        print(user_outputs[2])
     if input_ == commands[0]:
         print(user_outputs[0])
         keep_going = False
     elif input_ == commands[1]:
         print(user_outputs[1])
-    elif is_empty_line(input_):
-        pass
-    elif is_single_number(input_):
-        print(input_)
-    else:        
-        print(sum(convert_input(input_)))
+
+def input_guardian(input_):
+    """ Checks if the input format is technically correct
+
+    If the input starts with "/" it will be treated as a command
+    If the input is only digits it will be printed as is later on
+    The input cannot end with an operator symbol
+    Otherwise there should be at least one of the operator symbols in the input
+    """
+    if input_.startswith(command_start_symbol):
+        return True
+    if input_.isdigit():
+        return True
+    if input_.endswith(operator_symbols[0]):
+        return False
+    if input_.endswith(operator_symbols[1]):
+        return False
+    if (operator_symbols[0] not in input_) and (operator_symbols[1] not in input_):
+        return False
+    return True
+
+def input_handler(input_):
+    """ Checks the user input and passes it to the correct function
+    
+    The guardian will check if the input format is
+    input that starts with "/" has to go to the command function
+    If there is no empty space in the input it will be printed as is, otherwise the sum of all items is calculated
+    """
+    global keep_going
+    if input_guardian(input_):
+        if input_.startswith(command_start_symbol):
+            command_handler(input_)
+        elif is_empty_line(input_):
+            pass
+        elif is_single_number(input_):
+            print(input_)
+        else:        
+            print(sum(convert_input(input_)))
+    if not input_guardian(input_):
+        print(user_outputs[3])
 
 def calculator_loop():
     global keep_going
@@ -85,3 +127,12 @@ def calculator_loop():
         input_handler(input_)
 
 calculator_loop()
+
+#print(input_guardian('11'))
+#print(input_guardian('8 + 7 - 4'))
+#print(input_guardian('abc'))
+#print(input_guardian('+15'))
+#print(input_guardian('18 22'))
+#print(input_guardian('-22'))
+#print(input_guardian('22-'))
+#print(input_guardian('/go'))
