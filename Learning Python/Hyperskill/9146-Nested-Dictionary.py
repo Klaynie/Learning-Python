7f7b39d33af523e83c75e3803dada63e327afbc1
Christoph Lenz <coiz@hotmail.de>
2020-06-03 10:33:30 +0200

    First Version of 9146

---
+++ Learning Python/Hyperskill/9146-Nested-Dictionary.py
@@ -0,0 +1,7 @@
+children = {'Emily': 'artist', 'Adam': 'astronaut', 'Nancy': 'programmer'}
+
+children['Emily'] = {'profession': 'artist', 'age': 5}
+children['Adam'] = {'profession': 'astronaut', 'age': 9}
+children['Nancy'] = {'profession': 'programmer', 'age': 14}
+
+print(children)
