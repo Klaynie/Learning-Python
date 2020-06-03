0aba5192648516ccc6c514889e1f28cb6ebe0cc5
Christoph Lenz <coiz@hotmail.de>
2020-06-03 18:00:18 +0200

    Refactor of 9553

--- Learning Python/Hyperskill/9553-Congratulatory-letter.py
+++ Learning Python/Hyperskill/9553-Congratulatory-letter.py
@@ -1,14 +1,4 @@
-def congratulations(*names):
-    print("Happy New Year! Take care of yourself and your loved ones!\nFor:")
-    name_list = []
-    for name in names:
-        if ' ' in name:
-            part = name.split()
-            for name in part:
-                name_list.append(name)
-        elif ' ' not in name:
-            name_list.append(name)
-    for name in name_list:
-       print(name)
+def congratulations(*employees):
+    print("Happy New Year! Take care of yourself and your loved ones!", "For:", *employees, sep="\n")
 
 congratulations("Alice", "Mike", "Roman Victoria")
