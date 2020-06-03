6283edf66ce1890de3bfcec89d0beb8c1a0da54f
Christoph Lenz <coiz@hotmail.de>
2020-06-03 18:24:15 +0200

    First Version of 6553

---
+++ Learning Python/Hyperskill/6553-All-hands-on-deck.py
@@ -0,0 +1,17 @@
+hand_value = 0
+card_coutner = 0
+card_values = {'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+
+def sum_card_value(card):
+    global hand_value, card_values
+    if card.isdigit():
+        hand_value += int(card)
+    else:
+        hand_value += card_values[card]
+
+while card_coutner < 6:
+    card = input()
+    sum_card_value(card)
+    card_coutner += 1
+
+print(hand_value/card_coutner)
