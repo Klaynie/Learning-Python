16c88ac14037198a01163c71f72d8a71a3af9576
Christoph Lenz <coiz@hotmail.de>
2020-06-04 11:37:20 +0200

    Refactoring of 6490

--- Learning Python/Hyperskill/6490-Extreme-Points.py
+++ Learning Python/Hyperskill/6490-Extreme-Points.py
@@ -1,17 +1,9 @@
 test_dict = {"a": 43, "b": 1233, "c": 8}
 
-min_value = 100000
-max_value = 0
-min_name = ''
-max_name = ''
+for k, v in test_dict.items():
+    if v == min(test_dict.values()):
+        print(f'min: {k}')
 
-for key, value in test_dict.items():
-    if value > max_value:
-        max_value = value
-        max_name = key
-    elif value < min_value:
-        min_value = value
-        min_name = key
-
-print(f'min: {min_name}')
-print(f'max: {max_name}')
+for k, v in test_dict.items():
+    if v == max(test_dict.values()):
+        print(f'max: {k}')
