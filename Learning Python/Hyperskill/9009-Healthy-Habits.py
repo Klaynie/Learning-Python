68618874e89724609b7b13300e9aee8a4cc3f8f2
Christoph Lenz <coiz@hotmail.de>
2020-06-04 11:46:01 +0200

    First Version of 9009

---
+++ Learning Python/Hyperskill/9009-Healthy-Habits.py
@@ -0,0 +1,14 @@
+walks = [
+    {"day": "Monday", "distance": 2000},
+    {"day": "Tuesday", "distance": 3000},
+    {"day": "Wednesday", "distance": 3500},
+    {"day": "Thursday", "distance": 2500},
+    {"day": "Friday", "distance": 2500},
+    {"day": "Saturday", "distance": 1000},
+    {"day": "Sunday", "distance": 5600}]
+
+distance_total = 0
+for item in walks:
+    distance_total += item['distance']
+
+print(round(distance_total / len(walks)))
