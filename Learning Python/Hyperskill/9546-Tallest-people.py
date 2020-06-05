49f1b04944a83b9318b84657efa994235e5a9523
Christoph Lenz <coiz@hotmail.de>
2020-06-05 10:26:55 +0200

    Refactor of 9546

--- Learning Python/Hyperskill/9546-Tallest-people.py
+++ Learning Python/Hyperskill/9546-Tallest-people.py
@@ -1,10 +1,5 @@
-def tallest_people(**names):
-    tallest_people_list = []
-    for name, height in names.items():
-        if height == max(names.values()):
-            tallest_people_list.append(f"{name} : {height}")
-    tallest_people_list.sort()
-    for item in tallest_people_list:
-        print(item)
+def tallest_people(**people):
+    print('\n'.join(f'{name} : {people[name]}' for name in sorted(people)
+                    if people[name] == max(people.values())))
 
 tallest_people(Jackie=176, Wilson=185, Saersha=165, Roman=185, Abram=169)
