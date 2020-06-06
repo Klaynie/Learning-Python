fa1e3e8d443efb61a4a8198550fe265b5562a04e
Christoph Lenz <coiz@hotmail.de>
2020-06-06 23:15:45 +0200

    Refactoring of Code

--- Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
+++ Learning Python/Hyperskill/Smart-Calculator/Smart-Calculator.py
@@ -1,5 +1,29 @@
 import string
+from enum import IntEnum
 from collections import deque
+
+class UserOutput(IntEnum):
+    BYE = 0
+    EXPLAIN = 1
+    UNKNOW_COMMAND = 2
+    INVALID_EXPRESSION = 3
+    INVALID_IDENTIFIER = 4
+    UNKNOW_VARIABLE = 5
+    INVALID_ASSIGNMENT = 6
+
+class OperatorSymbol(IntEnum):
+    PLUS = 0
+    MINUS = 1
+    TIMES = 2
+    DIVISION = 3
+    POWER = 4
+
+class EqualitySymbol(IntEnum):
+    EQUAL = 0
+
+class CommandKeyword(IntEnum):
+    EXIT = 0
+    HELP = 1
 
 commands = ['/exit','/help']
 user_outputs = ['Bye!',
@@ -10,7 +34,8 @@ user_outputs = ['Bye!',
                 'Unknown variable',
                 'Invalid assignment']
 command_start_symbol = '/'
-operator_symbols = ['+', '-', '=', '*', '/', '^']
+equality_symbols = ['=']
+operator_symbols = ['+', '-', '*', '/', '^']
 bracket_symbols = ['(', ')']
 variables_dict = {}
 keep_going = True
@@ -24,9 +49,9 @@ def is_single_number(input_):
 def split_input_into_assignment(input_):
     input_ = input_.replace(' ', '')
     try:
-        key, value = input_.split(operator_symbols[2])
+        key, value = input_.split(equality_symbols[EqualitySymbol.EQUAL])
     except ValueError:
-        print(user_outputs[6])
+        print(user_outputs[UserOutput.INVALID_ASSIGNMENT])
     else:
         return key, value
 
@@ -48,30 +73,33 @@ def assign_variable(key, value):
         try:
             variables_dict[value]
         except KeyError:
-            print(user_outputs[5]) # Variable on right side of assignment is not defined
+            print(user_outputs[UserOutput.UNKNOW_VARIABLE])
         else:
             variables_dict[key] = variables_dict[value]
     else:
         variables_dict[key] = int(convert_single_number(value))
 
 def check_assignment(key, value):
-    """ Will if the assignment can be performed, otherwise inform the user what went wrong.
+    """ Will check if the assignment can be performed, otherwise inform the user what went wrong.
 
     key, value are derived from user input via split_input_into_assignment(input_) and need to be checked for validity
     """
+    result = False
     if check_assignment_validity(key, value):
-        return True
-    if not check_valid_variable_name(key):
-        print(user_outputs[4]) # Variable name is not valid
-        return False
-    if not check_valid_content(key, value):
-        print(user_outputs[6]) # Assignment is not valid
-        return False
-    if value not in variables_dict.values():
-        print(user_outputs[5]) # Variable on right side of assignment is not defined
-        return False
-    print(user_outputs[6]) # Fallback
-    return False
+        result = True
+    elif not check_valid_variable_name(key):
+        print(user_outputs[UserOutput.INVALID_IDENTIFIER])
+        result = False
+    elif not check_valid_content(key, value):
+        print(user_outputs[UserOutput.INVALID_ASSIGNMENT])
+        result = False
+    elif value not in variables_dict.values():
+        print(user_outputs[UserOutput.UNKNOW_VARIABLE])
+        result = False
+    else:
+        print(user_outputs[UserOutput.INVALID_ASSIGNMENT])
+    
+    return result
 
 def check_assignment_validity(key, value):
     return check_valid_variable_name(key) and check_valid_content(key, value)
@@ -84,13 +112,16 @@ def check_valid_variable_name(input_):
 
 def check_valid_content(key, value):
     global variables_dict
+    result = False
+
     if value.isdigit():
-        return True
-    if try_conversion(value):
-        return True
-    if check_valid_variable_name(value):
-        return True
-    return False
+        result = True
+    elif try_conversion(value):
+        result = True
+    elif check_valid_variable_name(value):
+        result = True
+
+    return result
 
 def try_conversion(value):
     for item in value:
@@ -114,9 +145,9 @@ def print_variable_content(input_):
     if input_ in variables_dict:
         print(variables_dict[input_])
     elif not check_valid_variable_name(input_):
-        print(user_outputs[4]) # Invalid identifier
+        print(user_outputs[UserOutput.INVALID_IDENTIFIER])
     else:
-        print(user_outputs[5]) # Unknown variable
+        print(user_outputs[UserOutput.UNKNOW_VARIABLE])
 
 def convert_single_number(input_):
     global operator_symbols
@@ -128,7 +159,7 @@ def convert_single_number(input_):
         elif item.isdigit():
             number += item
     sign = convert_operator_string(operator_string)
-    if sign == operator_symbols[0]:
+    if sign == operator_symbols[OperatorSymbol.PLUS]:
         sign = ''
     return sign+number
 
@@ -159,16 +190,19 @@ def generate_output_list(numbers, operators):
         convert_numbers_and_operators(start=1, end=len(numbers), numbers=numbers, operators=operators, output_list=output_list)
     return output_list
 
+def contains_plus_or_minus(string):
+    return operator_symbols[OperatorSymbol.PLUS] in string or operator_symbols[OperatorSymbol.MINUS] in string
+
 def convert_input(input_):
     global operator_symbols, variables_dict
     """ Converts the user input to be usable for calculations
     
     The user input needs to be converted into operators and numbers
     """
-    if input_.startswith(operator_symbols[0]):
-        input_ = input_.replace(operator_symbols[0], operator_symbols[0] + ' ', 1)
-    elif input_.startswith(operator_symbols[1]):
-        input_ = input_.replace(operator_symbols[1], operator_symbols[1] + ' ', 1)
+    if input_.startswith(operator_symbols[OperatorSymbol.PLUS]):
+        input_ = input_.replace(operator_symbols[OperatorSymbol.PLUS], operator_symbols[OperatorSymbol.PLUS] + ' ', 1)
+    elif input_.startswith(operator_symbols[OperatorSymbol.MINUS]):
+        input_ = input_.replace(operator_symbols[OperatorSymbol.MINUS], operator_symbols[OperatorSymbol.MINUS] + ' ', 1)
     items = [item for item in input_.split()]
     numbers = []
     operators = []
@@ -179,7 +213,10 @@ def convert_input(input_):
             try:
                 variables_dict[item]
             except KeyError:
-                operators.append(convert_operator_string(item))
+                if contains_plus_or_minus(item):
+                    operators.append(convert_operator_string(item))
+                else:
+                    print(user_outputs[UserOutput.UNKNOW_VARIABLE])
             else:
                 numbers.append(variables_dict[item])
     return generate_output_list(numbers, operators)
@@ -226,17 +263,19 @@ def convert_operator_string(operator_string):
     
     The number of minus signs defines the algebraic sign of the number
     """
-    if operator_symbols[1] not in operator_string:
-        return operator_symbols[0]
+    if not contains_plus_or_minus(operator_string):
+        return operator_string
+    elif operator_symbols[OperatorSymbol.MINUS] not in operator_string:
+        return operator_symbols[OperatorSymbol.PLUS]
     else:
         count_minus_sign = 0
         for sign in operator_string:
-            if sign == operator_symbols[1]:
+            if sign == operator_symbols[OperatorSymbol.MINUS]:
                 count_minus_sign += 1
         if count_minus_sign % 2 == 0:
-            return operator_symbols[0]
+            return operator_symbols[OperatorSymbol.PLUS]
         else:
-            return operator_symbols[1]
+            return operator_symbols[OperatorSymbol.MINUS]
 
 def command_handler(input_):
     """ Checks if the command is in the list of commands
@@ -245,12 +284,12 @@ def command_handler(input_):
     """
     global keep_going, commands
     if input_ not in commands:
-        print(user_outputs[2])
-    if input_ == commands[0]:
-        print(user_outputs[0])
+        print(user_outputs[UserOutput.UNKNOW_COMMAND])
+    if input_ == commands[CommandKeyword.EXIT]:
+        print(user_outputs[UserOutput.BYE])
         keep_going = False
-    elif input_ == commands[1]:
-        print(user_outputs[1])
+    elif input_ == commands[CommandKeyword.HELP]:
+        print(user_outputs[UserOutput.EXPLAIN])
 
 def input_guardian(input_):
     """ Checks if the input format is technically correct
@@ -260,17 +299,37 @@ def input_guardian(input_):
     The input cannot end with an operator symbol
     Otherwise there should be at least one of the operator symbols in the input
     """
+    result = True
+
     if input_.startswith(command_start_symbol):
-        return True
-    if input_.isdigit():
-        return True
-    if is_empty_line(input_):
-        return True
-    if input_.endswith(operator_symbols[0]):
-        return False
-    if input_.endswith(operator_symbols[1]):
-        return False
-    return True
+        result = True
+    elif input_.isdigit():
+        result = True
+    elif is_empty_line(input_):
+        result = True
+    elif input_.endswith(operator_symbols[OperatorSymbol.PLUS]):
+        result = False
+    elif input_.endswith(operator_symbols[OperatorSymbol.MINUS]):
+        result = False
+
+    return result
+
+def contains_operator_symbols(input_):
+    global operator_symbols
+    result = False
+
+    if operator_symbols[OperatorSymbol.PLUS] in input_:
+        result = True
+    elif operator_symbols[OperatorSymbol.MINUS] in input_:
+        result = True
+    elif operator_symbols[OperatorSymbol.TIMES] in input_:
+        result = True
+    elif operator_symbols[OperatorSymbol.DIVISION] in input_:
+        result = True
+    elif operator_symbols[OperatorSymbol.POWER] in input_:
+        result = True
+    
+    return result
 
 def input_handler(input_):
     """ Checks the user input and passes it to the correct function
@@ -285,16 +344,16 @@ def input_handler(input_):
             command_handler(input_)
         elif is_empty_line(input_):
             pass
-        elif check_for_variables(input_) and operator_symbols[0] not in input_ and operator_symbols[1] not in input_ and operator_symbols[2] not in input_:
-            print_variable_content(input_)
-        elif operator_symbols[2] in input_:
+        elif equality_symbols[EqualitySymbol.EQUAL] in input_:
             variable_assignment(input_)
+        elif check_for_variables(input_) and not contains_operator_symbols(input_):
+            print_variable_content(input_)
         elif is_single_number(input_):
             print(convert_single_number(input_))
         else:        
             print(sum(convert_input(input_)))
-    if not input_guardian(input_):
-        print(user_outputs[3])
+    elif not input_guardian(input_):
+        print(user_outputs[UserOutput.INVALID_EXPRESSION])
 
 def calculator_loop():
     global keep_going
