2326bde61431bf9495586a06feae801d86a9e557
Christoph Lenz <coiz@hotmail.de>
2020-06-06 15:57:45 +0200

    Refactoring of Hangman during Code Review

--- Learning Python/Hyperskill/Hangman/Hangman.py
+++ Learning Python/Hyperskill/Hangman/Hangman.py
@@ -1,16 +1,30 @@
 import random
+from enum import IntEnum
+import re
+
+class OutcomeMessage(IntEnum):
+    NO_IMPROVEMENTS = 0
+    NO_SUCH_LETTER = 1
+    YOU_GUESSED_RIGHT = 2
+
+class GuardianMessage(IntEnum):
+    ONLY_SINGLE_LETTER = 0
+    ONLY_ASCII = 1
+    ALREADY_TYPED = 2
 
 title = 'H A N G M A N'
 user_messages = ['Input a letter: ', 'Type "play" to play the game, "exit" to quit:']
 play_keyword = 'play'
 exit_keyword = 'exit'
-outcome_messages = ['No improvements', 'No such letter in the word','You guessed the word!', 'You survived!', 'You are hanged!']
-guardian_messages = ['You should input a single letter', 'It is not an ASCII lowercase letter', 'You already typed this letter']
+outcome_messages = ['No improvements', 'No such letter in the word','You guessed the word!', 'You survived!', 'You are hanged!'] #Enum
+guardian_messages = ['You should input a single letter', 'It is not an ASCII lowercase letter', 'You already typed this letter'] #Enum
 ascii_letters = 'abcdefghijklmnopqrstuvwxyz'
 word_list = ['python', 'java', 'kotlin', 'javascript']
 placeholder_character = '-'
-max_lives = 8
-strikes = 0
+max_lives = 8 #Ausgeben
+
+# Ab hier sind es Dinge, die sich in der Laufzeit ändern
+strikes = 0 #Strikes anzeigen
 correctly_guessed_letters = set()
 guessed_letters = set()
 word_to_guess_set = set()
@@ -25,18 +39,26 @@ def set_word_to_guess():
 def is_letter_in_word_to_guess(input_):
     return input_ in word_to_guess_set
 
-def input_guardian(input_):
+def print_guardian_message(int_enum): # Generalisieren auf Print Message
+    print(guardian_messages[int_enum])
+
+def has_more_than_one_letter(input_):
+    return len(input_) != 1
+
+def input_guardian(input_): # Maximal 3 Values, sonst DTO/Type
     global ascii_letters, guessed_letters
-    if len(input_) != 1:
-        print(guardian_messages[0])
-        return False
-    if input_ not in ascii_letters:
-        print(guardian_messages[1])
-        return False
-    if input_ in guessed_letters:
-        print(guardian_messages[2])
-        return False
-    return True
+    result = False
+
+    if has_more_than_one_letter(input_):
+        print_guardian_message(GuardianMessage.ONLY_SINGLE_LETTER) # Generalisieren auf Print Message
+    elif input_ not in ascii_letters:
+        print_guardian_message(GuardianMessage.ONLY_ASCII)
+    elif input_ in guessed_letters:
+        print_guardian_message(GuardianMessage.ALREADY_TYPED)
+    else:
+        result = True
+
+    return result # Immer nur ein Return Value
 
 def input_handler(input_):
     global user_output_word, guessed_letters, correctly_guessed_letters, strikes
@@ -44,7 +66,7 @@ def input_handler(input_):
         add_letter_to_guessed_letters(input_)
         if is_letter_in_word_to_guess(input_):
             add_letter_to_corretcly_guessed_letters(input_)
-            generate_user_output_word()
+            generate_user_output_word2()
         else:
             strikes += 1
             print(outcome_messages[1])
@@ -73,6 +95,19 @@ def generate_user_output_word():
     user_output_word = convert_list_to_string(user_output_word)
     return user_output_word
 
+def generate_user_output_word2():
+    global word_to_guess, user_output_word, correctly_guessed_letters, placeholder_character, ascii_letters
+    regular_expr = get_regular_expression_for_user_output(user_output_word, correctly_guessed_letters)
+    result = re.sub(regular_expr, placeholder_character, word_to_guess)
+    user_output_word = result
+    return result # Immer nur ein Return Value
+
+def get_regular_expression_for_user_output(user_output_word, correctly_guessed_letters):
+    result = '[' + ascii_letters + ']'
+    for letter in correctly_guessed_letters:
+        result = result.replace(letter, '')
+    return result
+
 def generate_word_to_guess_set():
     global word_to_guess
     return set(word_to_guess)
@@ -91,30 +126,41 @@ def output_loss():
     print('\n')
 
 def output_win():
-    print(outcome_messages[2])
+    print(word_to_guess)
+    print(outcome_messages[OutcomeMessage.YOU_GUESSED_RIGHT])
     print(outcome_messages[3])
     print('\n')
 
-def game_loop():
-    global max_lives, strikes, user_output_word, guessed_letters, correctly_guessed_letters
+def print_user_output_word():
+    print('\n')
+    print(user_output_word)
+
+def session_loop():
+    initiate_session()
+    stay_in_session = True
+    while stay_in_session:
+        print_user_output_word() #Always print user output word after guessing
+        input_handler(input(user_messages[0]))
+        if strikes == max_lives:
+            stay_in_session = False
+            output_loss() #Always print user output word after guessing
+        elif word_to_guess == user_output_word:
+            stay_in_session = False
+            output_win() #Always print user output word after guessing
+
+def main_loop():
     stay_in_game = True
     while stay_in_game:
         start_or_stop = input(user_messages[1])
         if start_or_stop == play_keyword:
-            initiate_session()
-            stay_in_session = True
-            while stay_in_session:
-                print('\n')
-                print(user_output_word)
-                input_handler(input(user_messages[0]))
-                if strikes == max_lives:
-                    stay_in_session = False
-                    output_loss()
-                elif word_to_guess == user_output_word:
-                    stay_in_session = False
-                    output_win()
+            session_loop()
         elif start_or_stop == exit_keyword:
             stay_in_game = False
+
+def game_loop():
+    global max_lives, strikes, user_output_word, guessed_letters, correctly_guessed_letters
+    main_loop()
+    
 
 #Start the Game
 print(title)
