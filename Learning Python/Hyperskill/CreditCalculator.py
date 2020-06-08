0c9bbce3a435c09fffa299bc427906301615e735
Christoph Lenz <coiz@hotmail.de>
2020-06-08 14:55:19 +0200

    First Version of Project Stage 2

--- Learning Python/Hyperskill/CreditCalculator.py
+++ Learning Python/Hyperskill/CreditCalculator.py
@@ -11,11 +11,13 @@ class Keyword(IntEnum):
     MONTHS = 0
     PAYMENT = 1
 
-class UserMessage(IntEnum):
-    MONTHLY_PAYMENTS_EVEN = 0
-    MONTHLY_PAYMENTS_ODD = 1
-    COUNT_OF_MONTHS_1 = 2
-    COUNT_OF_MONTHS_N = 3
+class MonthMessage(IntEnum):
+    COUNT_OF_MONTHS_1 = 0
+    COUNT_OF_MONTHS_N = 1
+
+class PaymentMessage(IntEnum):
+    EVEN_MONTHLY_PAYMENT = 0
+    ADJUSTED_PAYMENT = 1
 
 keywords = ['m', 'p']
 user_prompts = [f'What do you want to calculate?\n\
@@ -26,58 +28,76 @@ type "{keywords[Keyword.PAYMENT]}" - for monthly payment:',
 'Enter count of months:'\
 ]
 
-def print_monthly_payment_messages(monthly_payment_even = 0, monthly_payment_odd = 0):
-    monthly_payment_messages = [f'Your monthly payment = {monthly_payment_even:.d}',
-                     f'Your monthly payment = {monthly_payment_even:.d} with last month payment = {monthly_payment_odd:.d}',
-                    ]
-    print(monthly_payment_messages[0])
-
-def print_count_of_month_messages(count_of_months = 1):
-    count_of_month_messages = [f'It takes {count_of_months} month to repay the credit',
-                               f'It takes {count_of_months} months to repay the credit'
-                            ]
-    print(count_of_month_messages[0])
-
-def calculate_count_of_months(principal = 1000, monthly_payment = 1000):
-    return 1
-
-def calculate_monthly_payments(principal = 1000, count_of_months = 9):
-    monthly_payment_even = math.ceil(principal / count_of_months)
-    monthly_payment_odd = math.ceil(principal - (count_of_months - 1) *  monthly_payment_even)
-    return monthly_payment_even, monthly_payment_odd
-
 def get_value(prompt):
     return input(prompt + '\n')
 
+def get_monthly_payment_message(monthly_payment = 100, monthly_payment_last_month_adjustment = 0):
+    result = ''
+    monthly_payment_messages = [f'Your monthly payment = {monthly_payment:d}',
+                                f'Your monthly payment = {monthly_payment:d} with last month payment = {monthly_payment + monthly_payment_last_month_adjustment:d}',
+                              ]
+    if monthly_payment_last_month_adjustment == 0:
+        result = monthly_payment_messages[PaymentMessage.EVEN_MONTHLY_PAYMENT]
+    else:
+        result = monthly_payment_messages[PaymentMessage.ADJUSTED_PAYMENT]
+    return result
+
+def get_count_of_month_message(count_of_months = 1):
+    result = ''
+    count_of_month_messages = [f'It takes {count_of_months} month to repay the credit',
+                               f'It takes {count_of_months} months to repay the credit'
+                            ]
+    if count_of_months == 1:
+        result = count_of_month_messages[MonthMessage.COUNT_OF_MONTHS_1]
+    else:
+        result = count_of_month_messages[MonthMessage.COUNT_OF_MONTHS_N]
+    return result
+
+def calculate_count_of_month(principal = 1000, monthly_payment = 1000):
+    return math.ceil(principal / monthly_payment)
+
+def calculate_monthly_payment(principal = 1000, count_of_months = 9):
+    monthly_payment = math.ceil(principal / count_of_months)
+    monthly_payment_last_month_adjustment =  math.ceil(principal - (count_of_months - 1) *  monthly_payment) - monthly_payment
+    return monthly_payment, monthly_payment_last_month_adjustment
+
 def monthly_payment_calculator(principal):
     count_of_months = get_value(user_prompts[UserPrompt.COUNT_OF_MONTHS])
-    monthly_payment_even, monthly_payment_odd = calculate_monthly_payments(principal, count_of_months)
-    print_monthly_payment_messages(monthly_payment_even, monthly_payment_odd)
+    monthly_payment, monthly_payment_last_month_adjustment = calculate_monthly_payment(principal, int(count_of_months))
+    result = get_monthly_payment_message(monthly_payment, monthly_payment_last_month_adjustment)
+    return result
 
 def count_of_months_calculator(principal):
     monthly_payment = get_value(user_prompts[UserPrompt.MONTHLY_PAYMENT])
-    count_of_months = calculate_count_of_months(principal, monthly_payment)
-    print_count_of_month_messages(count_of_months)
+    count_of_months = calculate_count_of_month(principal, int(monthly_payment))
+    result = get_count_of_month_message(count_of_months)
+    return result
 
 def start_calculation(keyword, principal):
+    result = ''
     if keyword == keywords[Keyword.MONTHS]:
-        count_of_months_calculator(principal)
+        result = count_of_months_calculator(principal)
     elif keyword == keywords[Keyword.PAYMENT]:
-        monthly_payment_calculator(principal)
+        result = monthly_payment_calculator(principal)
+    return result
+
+def print_result(result):
+    print('\n')
+    print(result)
 
 def request_inputs():
     principal = get_value(user_prompts[UserPrompt.PRINCIPAL])
     keyword = get_value(user_prompts[UserPrompt.KEYWORD])
-    start_calculation(keyword, principal)
+    return principal, keyword
 
 def calculator_loop():
     global keywords, user_prompts
     keep_going = True
     while keep_going:
-        request_inputs()
+        principal, keyword = request_inputs()
+        result = start_calculation(keyword, int(principal))
+        print_result(result)
         keep_going = False
 
-#calculator_loop()
-print(calculate_monthly_payments())
-print(calculate_monthly_payments())
-print(calculate_monthly_payments())
+if __name__ == "__main__":
+    calculator_loop()
--- Learning Python/Unit Tests/CreditCalculator_test.py
+++ Learning Python/Unit Tests/CreditCalculator_test.py
@@ -5,4 +5,32 @@ import unittest
 
 class CalculatePaymentCases(unittest.TestCase):
     def test_calculate_payment_1(self):
-        self.assertTrue(calculate_monthly_payments() == (112, 104))
+        self.assertEqual(calculate_monthly_payment(), (112, -8))
+
+    def test_calculate_payment_2(self):
+        principal = 1000
+        months = 10
+        self.assertEqual(calculate_monthly_payment(principal, months), (100, 0))
+
+class CalculateMonthCases(unittest.TestCase):
+    def test_calculate_month_1(self):
+        self.assertEqual(calculate_count_of_month(), 1)
+
+    def test_calculate_month_2(self):
+        principal = 1000
+        payment = 150
+        self.assertEqual(calculate_count_of_month(principal, payment), 7)
+
+class PrintPaymentCases(unittest.TestCase):
+    def test_print_payment_1(self):
+        self.assertEqual(get_monthly_payment_message(), 'Your monthly payment = 100')
+
+    def test_print_payment_2(self):
+        self.assertEqual(get_monthly_payment_message(112, -8), 'Your monthly payment = 112 with last month payment = 104')
+
+class PrintMonthCases(unittest.TestCase):
+    def test_print_months_1(self):
+        self.assertEqual(get_count_of_month_message(), 'It takes 1 month to repay the credit')
+
+    def test_print_months_2(self):
+        self.assertEqual(get_count_of_month_message(2), 'It takes 2 months to repay the credit')
