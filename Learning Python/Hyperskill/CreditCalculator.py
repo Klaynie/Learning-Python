67638e670953332c16770954b25e01b57d4c4a91
Christoph Lenz <coiz@hotmail.de>
2020-06-08 18:27:53 +0200

    Test Cases Setup for Project Stage 3

--- Learning Python/Hyperskill/CreditCalculator.py
+++ Learning Python/Hyperskill/CreditCalculator.py
@@ -5,97 +5,99 @@ class UserPrompt(IntEnum):
     KEYWORD = 0
     PRINCIPAL = 1
     MONTHLY_PAYMENT = 2
-    COUNT_OF_MONTHS = 3
+    MONTHS = 3
+    INTEREST = 4
 
 class Keyword(IntEnum):
     MONTHS = 0
     PAYMENT = 1
+    PRINCIPAL = 2
 
-class MonthMessage(IntEnum):
-    COUNT_OF_MONTHS_1 = 0
-    COUNT_OF_MONTHS_N = 1
-
-class PaymentMessage(IntEnum):
-    EVEN_MONTHLY_PAYMENT = 0
-    ADJUSTED_PAYMENT = 1
-
-keywords = ['m', 'p']
+keywords = ['m', 'a', 'p']
 user_prompts = [f'What do you want to calculate?\n\
 type "{keywords[Keyword.MONTHS]}" - for count of months,\n\
-type "{keywords[Keyword.PAYMENT]}" - for monthly payment:',
+type "{keywords[Keyword.PAYMENT]}" - for monthly payment:\n\
+type  "{keywords[Keyword.PRINCIPAL]}" - for credit principal:',
 'Enter the credit principal:',\
 'Enter monthly payment:',\
-'Enter count of months:'\
+'Enter count of periods:',\
+'Enter credit interest:'
 ]
 
 def get_value(prompt):
     return input(prompt + '\n')
 
-def get_monthly_payment_message(monthly_payment = 100, monthly_payment_last_month_adjustment = 0):
+def generate_message(number, variable):
     result = ''
-    monthly_payment_messages = [f'Your monthly payment = {monthly_payment:d}',
-                                f'Your monthly payment = {monthly_payment:d} with last month payment = {monthly_payment + monthly_payment_last_month_adjustment:d}',
-                              ]
-    if monthly_payment_last_month_adjustment == 0:
-        result = monthly_payment_messages[PaymentMessage.EVEN_MONTHLY_PAYMENT]
-    else:
-        result = monthly_payment_messages[PaymentMessage.ADJUSTED_PAYMENT]
+    messages = [f'You need {variable} to repay this credit!',\
+                f'Your annuity payment = {variable}!',\
+                f'Your credit principal = {variable}!'
+    ]
+    result = messages[number]
     return result
 
-def get_count_of_month_message(count_of_months = 1):
-    result = ''
-    count_of_month_messages = [f'It takes {count_of_months} month to repay the credit',
-                               f'It takes {count_of_months} months to repay the credit'
-                            ]
-    if count_of_months == 1:
-        result = count_of_month_messages[MonthMessage.COUNT_OF_MONTHS_1]
-    else:
-        result = count_of_month_messages[MonthMessage.COUNT_OF_MONTHS_N]
-    return result
+def calculate_paydown_time(principal, monthly_payment, interest):
+    return 1
 
-def calculate_count_of_month(principal = 1000, monthly_payment = 1000):
-    return math.ceil(principal / monthly_payment)
-
-def calculate_monthly_payment(principal = 1000, count_of_months = 9):
-    monthly_payment = math.ceil(principal / count_of_months)
-    monthly_payment_last_month_adjustment =  math.ceil(principal - (count_of_months - 1) *  monthly_payment) - monthly_payment
-    return monthly_payment, monthly_payment_last_month_adjustment
-
-def monthly_payment_calculator(principal):
-    count_of_months = get_value(user_prompts[UserPrompt.COUNT_OF_MONTHS])
-    monthly_payment, monthly_payment_last_month_adjustment = calculate_monthly_payment(principal, int(count_of_months))
-    result = get_monthly_payment_message(monthly_payment, monthly_payment_last_month_adjustment)
-    return result
-
-def count_of_months_calculator(principal):
+def paydown_time_calculator(principal, monthly_payment, interest):
+    principal = get_value(user_prompts[UserPrompt.PRINCIPAL])
     monthly_payment = get_value(user_prompts[UserPrompt.MONTHLY_PAYMENT])
-    count_of_months = calculate_count_of_month(principal, int(monthly_payment))
-    result = get_count_of_month_message(count_of_months)
+    interest = get_value(user_prompts[UserPrompt.INTEREST])
+    result = calculate_paydown_time(principal, monthly_payment, interest)
     return result
 
-def start_calculation(keyword, principal):
-    result = ''
+def calculate_principal(monthly_payment, months, interest):
+    return 1
+
+def principal_calculator():
+    monthly_payment = get_value(user_prompts[UserPrompt.MONTHLY_PAYMENT])
+    months = get_value(user_prompts[UserPrompt.MONTHS])
+    interest = get_value(user_prompts[UserPrompt.INTEREST])
+    result = calculate_principal(monthly_payment, months, interest)
+    return result
+
+def calculate_monthly_payment(principal, months, interest):
+    monthly_payment = 1
+    return monthly_payment
+
+def monthly_payment_calculator():
+    principal = get_value(user_prompts[UserPrompt.PRINCIPAL])
+    months = get_value(user_prompts[UserPrompt.MONTHS])
+    interest = get_value(user_prompts[UserPrompt.INTEREST])
+    result = calculate_monthly_payment(principal, months, interest)
+    return result
+
+def get_output_value(keyword):
+    result = 0
     if keyword == keywords[Keyword.MONTHS]:
-        result = count_of_months_calculator(principal)
+        result = paydown_time_calculator()
     elif keyword == keywords[Keyword.PAYMENT]:
-        result = monthly_payment_calculator(principal)
+        result = monthly_payment_calculator()
+    elif keyword == keywords[Keyword.PRINCIPAL]:
+        result = principal_calculator()
+    return result
+
+def get_number(keyword):
+    if keyword == keywords[Keyword.MONTHS]:
+        result = Keyword.MONTHS
+    elif keyword == keywords[Keyword.PAYMENT]:
+        result = Keyword.PAYMENT
+    elif keyword == keywords[Keyword.PRINCIPAL]:
+        result = Keyword.PRINCIPAL
     return result
 
 def print_result(result):
     print('\n')
     print(result)
 
-def request_inputs():
-    principal = get_value(user_prompts[UserPrompt.PRINCIPAL])
-    keyword = get_value(user_prompts[UserPrompt.KEYWORD])
-    return principal, keyword
-
 def calculator_loop():
     global keywords, user_prompts
     keep_going = True
     while keep_going:
-        principal, keyword = request_inputs()
-        result = start_calculation(keyword, int(principal))
+        keyword = get_value(user_prompts[UserPrompt.KEYWORD])
+        number = get_number(keyword)
+        variable = get_output_value(keyword)
+        result = generate_message(number, variable)
         print_result(result)
         keep_going = False
 
--- Learning Python/Unit Tests/CreditCalculator_test.py
+++ Learning Python/Unit Tests/CreditCalculator_test.py
@@ -3,34 +3,49 @@ sys.path.append('Hyperskill')
 from CreditCalculator import *
 import unittest
 
-class CalculatePaymentCases(unittest.TestCase):
-    def test_calculate_payment_1(self):
-        self.assertEqual(calculate_monthly_payment(), (112, -8))
+class GetNumberCases(unittest.TestCase):
+    def test_get_number_1(self):
+        self.assertEqual(get_number('m'), 0)
+    def test_get_number_2(self):
+        self.assertEqual(get_number('a'), 1)
+    def test_get_number_3(self):
+        self.assertEqual(get_number('p'), 2)
 
-    def test_calculate_payment_2(self):
-        principal = 1000
-        months = 10
-        self.assertEqual(calculate_monthly_payment(principal, months), (100, 0))
+class CalculatePaydownTimeCases(unittest.TestCase):
+    def test_calculate_paydown_time_1(self):
+        principal = 1000000
+        monthly_payment = 15000
+        interest = 10
+        self.assertEqual(calculate_paydown_time(principal, monthly_payment, interest), '8 years and 2 months')
 
-class CalculateMonthCases(unittest.TestCase):
-    def test_calculate_month_1(self):
-        self.assertEqual(calculate_count_of_month(), 1)
+class CalculateAnnuityPaymentCases(unittest.TestCase):
+    def test_calculate_annuity_payment_1(self):
+        principal = 1000000
+        months = 60
+        interest = 10
+        self.assertEqual(calculate_monthly_payment(principal, months, interest), 21248)
 
-    def test_calculate_month_2(self):
-        principal = 1000
-        payment = 150
-        self.assertEqual(calculate_count_of_month(principal, payment), 7)
+class CalculateCreditPrincipalCases(unittest.TestCase):
+    def test_calculate_credit_pricipal_1(self):
+        monthly_payment = 8721.8
+        months = 120
+        interest = 5.6
+        self.assertEqual(calculate_principal(monthly_payment, months, interest), 800000)
 
-class PrintPaymentCases(unittest.TestCase):
-    def test_print_payment_1(self):
-        self.assertEqual(get_monthly_payment_message(), 'Your monthly payment = 100')
+class PrintPaydownTimeCases(unittest.TestCase):
+    def test_paydown_time_1(self):
+        number = 0
+        variable = '8 years and 2 months'
+        self.assertEqual(generate_message(number, variable), 'You need 8 years and 2 months to repay this credit!')
 
-    def test_print_payment_2(self):
-        self.assertEqual(get_monthly_payment_message(112, -8), 'Your monthly payment = 112 with last month payment = 104')
+class PrintAnnuityPaymentCases(unittest.TestCase):
+    def test_annuity_payment_1(self):
+        number = 1
+        variable = 21248
+        self.assertEqual(generate_message(number, variable), 'Your annuity payment = 21248!')
 
-class PrintMonthCases(unittest.TestCase):
-    def test_print_months_1(self):
-        self.assertEqual(get_count_of_month_message(), 'It takes 1 month to repay the credit')
-
-    def test_print_months_2(self):
-        self.assertEqual(get_count_of_month_message(2), 'It takes 2 months to repay the credit')
+class PrintCreditPrincipalCases(unittest.TestCase):
+    def test_credit_principal_1(self):
+        number = 2
+        variable = 800000
+        self.assertEqual(generate_message(number, variable), 'Your credit principal = 800000!')
