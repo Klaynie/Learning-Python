43f5cf8ddf6d597bc5aa69a1ac431a331aa7889c
Christoph Lenz <coiz@hotmail.de>
2020-08-20 09:32:09 +0200

    Added more words

--- Learning-Python/Chapter08/Exercise812.py
+++ Learning-Python/Chapter08/Exercise812.py
@@ -1,29 +1,29 @@
 def rotate_word(word, amount):
-    startUpperCase = ord('A')
-    endUpperCase   = ord('Z')
-    startLowerCase = ord('a')
-    endLowerCase   = ord('z')
+    start_upper_case = ord('A')
+    end_upper_case   = ord('Z')
+    start_lower_case = ord('a')
+    end_lower_case   = ord('z')
     newWord = ''
     for letter in word:
         if letter.isnumeric():
             newLetter = letter
-        if letter.islower():
-            if (ord(letter)+amount <= endLowerCase and ord(letter)+amount >= startLowerCase):
-                newLetter = chr(ord(letter)+amount)
-            if ord(letter)+amount > endLowerCase:
-                newLetter = chr(startLowerCase+ord(letter)+amount-endLowerCase-1)
-            if ord(letter)+amount < startLowerCase:
-                additionalOrd = startLowerCase-(ord(letter)+amount)-1
-                newLetter = chr(endLowerCase-additionalOrd)
-        if letter.isupper():
-            if (ord(letter)+amount <= endUpperCase and ord(letter)+amount >= startUpperCase):
-                newLetter = chr(ord(letter)+amount)
-            if ord(letter)+amount < startUpperCase:
-                newLetter = chr(startUpperCase+ord(letter)+amount-endUpperCase-1)
-            if ord(letter)+amount < startUpperCase:
-                additionalOrd = startUpperCase-(ord(letter)+amount)-1
-                newLetter = chr(endUpperCase-additionalOrd)
-        newWord = newWord + newLetter
+        elif letter.islower():
+            new_letter = find_new_letter(letter, amount, start_lower_case, end_lower_case)
+        elif letter.isupper():
+            new_letter = find_new_letter(letter, amount, start_upper_case, end_upper_case)
+        newWord = newWord + new_letter
     return newWord
-        
-print(rotate_word('MELON',-10))
+
+def find_new_letter(letter, amount, start, end):
+    if (ord(letter) + amount <= end and ord(letter) + amount >= start):
+        new_letter = chr(ord(letter) + amount)
+    elif ord(letter) + amount > end:
+        new_letter = chr(start + ord(letter) + amount - end - 1)
+    elif ord(letter) + amount < start:
+        additional_ord = start-(ord(letter) + amount) - 1
+        new_letter = chr(end - additional_ord)
+    return new_letter
+
+print(rotate_word('cheer', 7))
+print(rotate_word('melon', -10))
+print(rotate_word('IBM', -1))
