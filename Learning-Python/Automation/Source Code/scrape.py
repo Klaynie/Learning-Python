f6583b23a6b593972f252855cb949d43dfa118f9
Christoph Lenz <coiz@hotmail.de>
2020-10-26 15:40:13 +0100

    Added Python Automation Code

---
+++ Learning-Python/Automation/Source Code/scrape.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://quotes.toscrape.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "lxml")
+quotes = soup.find_all("span", class_="text")
+authors = soup.find_all("small", class_="author")
+tags = soup.find_all("div", class_="tags")
+
+print(80 * "-")
+for i in range(0, len(quotes)):
+    print(quotes[i].text)
+    print(authors[i].text)
+    quote_tags = tags[i].find_all("a", class_="tag")
+    for quote_tag in quote_tags:
+        print(quote_tag.text)
+    print(80 * "-")
