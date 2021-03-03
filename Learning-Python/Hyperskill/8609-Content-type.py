b1513369f4963c1f19c0541098b4a8d1b58c9185
Christoph Lenz <coiz@hotmail.de>
2021-03-03 08:45:57 +0100

    Added Exercise

---
+++ Learning-Python/Hyperskill/8609-Content-type.py
@@ -0,0 +1,14 @@
+"""
+Define a function get_content_type(url) that, given a URL, sends a GET request and returns the content type of the response.
+
+For example, get_content_type('http://google.com') should return 'text/html; charset=ISO-8859-1', while get_content_type('http://github.com') returns 'text/html; charset=utf-8'.
+"""
+
+import requests
+
+
+def get_content_type(url):
+    return requests.get(url).headers['Content-Type']
+
+
+print(get_content_type('http://google.com'))
