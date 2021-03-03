77f6ad3e2cac2675bc59781a6129dfd0445526ea
Christoph Lenz <coiz@hotmail.de>
2021-03-03 16:19:26 +0100

    Added Solution for Stage 1/5

--- Learning-Python/Hyperskill/Web-Scraper/script.py
+++ Learning-Python/Hyperskill/Web-Scraper/script.py
@@ -1,12 +1,27 @@
 import requests
+import json
+from bs4 import BeautifulSoup
+
+
+def clean_raw_jason(raw_json):
+    raw_json = str(raw_json)
+    json_data = raw_json[raw_json.find('{'): raw_json.rfind('}') + 1]
+    return json_data
+
 
 url = input("Input the URL:\n")
 r = requests.get(url)
 if r:
-    json_data = r.json()
+    soup = BeautifulSoup(r.content, 'html.parser')
+    raw_json = soup.find('script', {"type": "application/ld+json"})
     try:
-        print(json_data['content'])
+        json_data = clean_raw_jason(raw_json)
+        movie_json = json.loads(json_data)
+        movie_data = {}
+        movie_data['name'] = movie_json['name']
+        movie_data['description'] = movie_json['description']
+        print(movie_data)
     except KeyError:
-        print("Invalid quote resource!")
+        print("Invalid movie page!")
 else:
-    print("Invalid quote resource!")
+    print("Invalid movie page!")
