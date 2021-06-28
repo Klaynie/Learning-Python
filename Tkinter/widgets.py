7578aaeddfd61b240a54013cfbd1a168193a5f22
Christoph Lenz <coiz@hotmail.de>
2021-06-28 16:35:27 +0200

    Added Tkinter Widgets Lesson

---
+++ Tkinter/widgets.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+button = ttk.Button(root, text='Click Me')
+button.pack()
+button.config(text='Push Me')
+ttk.Label(root, text='Hello, Tkinter!').pack()
+root.mainloop()
