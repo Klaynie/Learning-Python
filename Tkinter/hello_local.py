e4847a60fab1f9d9134ec65383c73544ef85903f
Christoph Lenz <coiz@hotmail.de>
2021-06-28 19:19:41 +0200

    Formatting

--- Tkinter/hello_local.py
+++ Tkinter/hello_local.py
@@ -3,15 +3,18 @@ from tkinter import *
 from tkinter import ttk
 from enum import IntEnum
 
+
 class ButtonName(IntEnum):
     RESET = 0
     TEXAS = 1
     HAWAII = 2
 
+
 class Greeting(IntEnum):
     DEFAULT = 0
     TEXAS = 1
     HAWAII = 2
+
 
 button_names = [
     'Reset',
@@ -25,30 +28,37 @@ greetings = [
     'Aloha, Tkinter'
 ]
 
+
 class HelloApp:
-    
+
     def __init__(self, master):
-        
+
         self.label = ttk.Label(master, text=greetings[Greeting.DEFAULT])
         self.label.grid(row=0, column=0, columnspan=2)
-        
-        ttk.Button(master, text=button_names[ButtonName.TEXAS], command=self.texas_hello).grid(row=1, column=0)
-        ttk.Button(master, text=button_names[ButtonName.HAWAII], command=self.hawaii_hello).grid(row=1, column=1)
-        ttk.Button(master, text=button_names[ButtonName.RESET], command=self.reset).grid(row=2, column=0, columnspan=2)
-        
+
+        ttk.Button(master, text=button_names[ButtonName.TEXAS], command=self.texas_hello).grid(
+            row=1, column=0)
+        ttk.Button(master, text=button_names[ButtonName.HAWAII], command=self.hawaii_hello).grid(
+            row=1, column=1)
+        ttk.Button(master, text=button_names[ButtonName.RESET], command=self.reset).grid(
+            row=2, column=0, columnspan=2)
+
     def texas_hello(self):
         self.label.config(text=greetings[Greeting.TEXAS])
-        
+
     def hawaii_hello(self):
         self.label.config(text=greetings[Greeting.HAWAII])
-        
+
     def reset(self):
         self.label.config(text=greetings[Greeting.DEFAULT])
 
+
 def main():
-    
+
     root = Tk()
     app = HelloApp(root)
     root.mainloop()
-    
-if __name__ == "__main__": main()
+
+
+if __name__ == "__main__":
+    main()
