3a1998854c368f60891642be4f3ed362e5e02055
Klaynie <54008218+Klaynie@users.noreply.github.com>
2021-12-09 18:53:59 +0100

    Fixed birthday calculation. Added birthday mesage.

--- Learning-Python/Chapter16/Exercise1607.py
+++ Learning-Python/Chapter16/Exercise1607.py
@@ -3,6 +3,7 @@ import math
 import copy
 
 def get_timestamp():
+    print(datetime.datetime.utcnow())
     return datetime.datetime.utcnow()
 
 def weekday_of_today():
@@ -10,7 +11,7 @@ def weekday_of_today():
     return timestamp.weekday()
 
 def calculate_age_between_two_dates(date1, date2):
-    return date2.year - date1.year - ((date2.month, date2.day) < (date1.month, date1.day))
+    return date2.year - date1.year - int(((date2.month, date2.day) < (date1.month, date1.day)))
 
 def current_age_from_birthday(birthday):
     return calculate_age_between_two_dates(birthday, get_timestamp())
@@ -22,13 +23,33 @@ def time_until_date(date, timestamp):
     return date - timestamp
 
 def get_time_to_next_birthday(birthday, currentTimestamp):
-    if birthday.month <= currentTimestamp.month and (birthday.month <= currentTimestamp.month and birthday.day <= currentTimestamp.day):
-        return time_until_date(datetime.datetime(currentTimestamp.year+1, birthday.month, birthday.day), currentTimestamp)
-    else:
+    if has_birthday_next_year(birthday, currentTimestamp):
         return time_until_date(datetime.datetime(currentTimestamp.year, birthday.month, birthday.day), currentTimestamp)
+    else:
+        return time_until_date(datetime.datetime(currentTimestamp.year + 1, birthday.month, birthday.day), currentTimestamp)
+
+def has_birthday_next_year(birthday, currentTimestamp):
+    result = True
+    if birthday.month < currentTimestamp.month:
+        result = False
+    else:
+        if birthday.day < currentTimestamp.day:
+            result = False
+    return result
+
+def has_birthday(birthday, currentTimestamp):
+    result = False
+    if birthday.month == currentTimestamp.month:
+        if birthday.day == currentTimestamp.day:
+            result = True
+    return result
 
 def print_age_and_time_to_next_birthday(birthday):
-    print('You are %g years old. Time to next birthday: %s' % (current_age_from_birthday(birthday), get_time_to_next_birthday(birthday, get_timestamp())))
+    currentTimestamp = get_timestamp()
+    if has_birthday(birthday, currentTimestamp):
+        print('You turn %g years old today. Happy Birthday! 🎉' % (current_age_from_birthday(birthday)))
+    else:
+        print('You are %g years old. Time to next birthday: %s' % (current_age_from_birthday(birthday), get_time_to_next_birthday(birthday, currentTimestamp)))
 
 def find_older_date(date1, date2):
     if date2 > date1:
@@ -45,7 +66,7 @@ def find_double_date(birthday1, birthday2, n):
         nextDayCounter += 1
     print(nextDay, age_until_date(olderBirthday, nextDay), age_until_date(youngerBirthday, nextDay))
 
-birthday1 = datetime.datetime(2000,11,15)
+birthday1 = datetime.datetime(2000,12,9)
 birthday2 = datetime.datetime(1977,2,2)
 
 print_age_and_time_to_next_birthday(birthday1)
